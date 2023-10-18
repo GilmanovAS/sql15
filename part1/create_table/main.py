516245b219dd993624241611482f19aed5bb6dc9
GilmanovAS <70641821+GilmanovAS@users.noreply.github.com>
2023-10-18 07:05:48 +0500

    set1

--- part1/create_table/main.py
+++ part1/create_table/main.py
@@ -15,22 +15,34 @@
 import sqlite3
 import prettytable
 
-con = sqlite3.connect(":memory:")
+con = sqlite3.connect("animals.db")
 cur = con.cursor()
-sqlite_query = ("")  # TODO составьте запрос на создание таблицы
+sqlite_query = ("""CREATE TABLE animals_color(
+Id integer PRIMARY KEY AUTOINCREMENT,
+Color1 integer FOREIGN KEY color(id) ON DELETE SET NULL ON UPDATE CASCADE,
+Color2 integer FOREIGN KEY color(id) ON DELETE SET NULL ON UPDATE CASCADE) """)
+# TODO составьте запрос на создание таблицы
+cur.execute(sqlite_query)
 
 # Не удаляйте код ниже, он используется
 # для вывода заголовков созданной таблицы
 
+# ("""CREATE TABLE animals_color(
+# Id integer PRIMARY KEY AUTOINCREMENT,
+# Color1 integer,
+# Color2 integer,
+# FOREIGN KEY (Color1) REFERENCES color(id) ON DELETE SET NULL ON UPDATE CASCADE,
+# FOREIGN KEY (Color2) REFERENCES color(id) ON DELETE SET NULL ON UPDATE CASCADE)  """
+# )
 
-def print_result(sqlite_query):
-    cur.execute(sqlite_query)
-    result_query = ('SELECT * from animals')
-    table = cur.execute(result_query)
-    mytable = prettytable.from_db_cursor(table)
-    mytable.max_width = 30
-    print(mytable)
-
-
-if __name__ == '__main__':
-    print_result(sqlite_query)
+# def print_result(sqlite_query):
+#     cur.execute(sqlite_query)
+#     result_query = ('SELECT * from animals')
+#     table = cur.execute(result_query)
+#     mytable = prettytable.from_db_cursor(table)
+#     mytable.max_width = 30
+#     print(mytable)
+#
+#
+# if __name__ == '__main__':
+#     print_result(sqlite_query)
